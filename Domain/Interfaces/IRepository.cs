48de58d92757a71f23a3c9a45f256f2cfeede6be
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-05-09 15:17:02 -0300

    feat(IRepository): add IRepository interface

---
+++ Domain/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IRepository<T>
+    {
+        T Create(T entity);
+        Task<T?> GetAsync(Expression<Func<T, bool>> predicate);
+        Task<IEnumerable<T>> GetAllAsync();
+        T Update(T entity);
+        T Delete(T entity);
+    }
+}
