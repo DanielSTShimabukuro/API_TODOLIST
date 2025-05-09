035c8801cfddf9fe4c33e0d96ba419a0628e4e52
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-05-09 15:25:41 -0300

    feat(DependencyInjection): add DependencyInjection configuration

---
+++ CrossCutting/IoC/DependencyInjection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Interfaces;
+using Domain.Interfaces.Items;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+using Infrastructure.Repositories.Items;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CrossCutting.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), mySqlOptions => mySqlOptions.EnableRetryOnFailure());
+            });
+
+            services.AddScoped<IItemRepository, ItemRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
