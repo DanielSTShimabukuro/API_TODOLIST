397a29601ec831a3d473fc2f6ba2aac7154b0b33
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-05-09 15:18:22 -0300

    feat(IItemRepository): add IItemRepository interface

---
+++ Domain/Interfaces/Items/IItemRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities.Items;
+
+namespace Domain.Interfaces.Items
+{
+    public interface IItemRepository : IRepository<Item>
+    {
+        
+    }
+}
