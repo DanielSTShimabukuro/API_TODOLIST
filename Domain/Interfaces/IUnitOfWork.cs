07b737738013184a435de1fa854cce6666785368
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-05-09 15:22:17 -0300

    feat(IUnitOfWork): add IUnitOfWork interface

---
+++ Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Interfaces.Items;
+
+namespace Domain.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        public IItemRepository Items { get; }
+
+        Task<int> CommitAsync();
+        
+        Task<int> CompleteAsync();
+
+        void Rollback();
+    }
+}
