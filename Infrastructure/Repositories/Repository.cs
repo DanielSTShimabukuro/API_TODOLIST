a12773caf43ff42204beb36eb006a3c443bce088
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-05-09 15:20:11 -0300

    feat(Repository): add generic Repository

---
+++ Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly AppDbContext _context;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public T Create(T entity)
+        {
+            _context.Set<T>().Add(entity);
+
+            return entity;
+        }
+
+        public T Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+
+            return entity;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().AsNoTracking().ToListAsync();
+        }
+
+        public async Task<T?> GetAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _context.Set<T>().FirstOrDefaultAsync(predicate);
+        }
+
+        public T Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+
+            return entity;
+        }
+    }
+}
