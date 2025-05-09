2336990951e39acec79d0cb7e148afe861dc39a6
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-05-09 15:21:20 -0300

    feat(ItemRepository): add ItemRepository

---
+++ Infrastructure/Repositories/Items/ItemRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities.Items;
+using Domain.Interfaces.Items;
+using Infrastructure.Data;
+
+namespace Infrastructure.Repositories.Items
+{
+    public class ItemRepository : Repository<Item>, IItemRepository
+    {
+        public ItemRepository(AppDbContext context) : base(context)
+        {
+
+        }
+    }
+}
