f5816a845e4837ee8e1519df48ba7a03d03cdab7
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-05-09 15:12:52 -0300

    feat(UpdateItemDTO): add UpdateItemDTO

---
+++ Application/DTOs/Items/UpdateItemDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Items
+{
+    public class UpdateItemDTO
+    {
+        [StringLength(100)]
+        public string? Name { get; set; }
+
+        [StringLength(100)]
+        public string? Description { get; set; }
+    }
+}
