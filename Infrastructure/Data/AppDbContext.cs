49e502b363862ca30cb4f196d02bc31d05c4409e
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-05-09 15:16:06 -0300

    feat(AppDbContext): add AppDbContext

---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities.Items;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
