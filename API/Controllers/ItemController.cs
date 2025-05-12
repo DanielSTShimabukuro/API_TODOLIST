79838b5d0b7aecd3d1a1bc88b859ea6a3d7a984d
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-05-12 15:05:19 -0300

    feat(ItemController): add CreateItem() function

---
+++ API/Controllers/ItemController.cs
@@ -0,0 +1,26 @@
+﻿using Application.DTOs.Items;
+using Application.Interfaces.Items;
+using Domain.Entities.Items;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        private readonly IItemService _itemService;
+
+        public ItemController(IItemService itemService)
+        {
+            _itemService = itemService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Item>> CreateItem(CreateItemDTO dto)
+        {
+            return Ok(await _itemService.CreateItem(dto));
+        }
+    }
+}
