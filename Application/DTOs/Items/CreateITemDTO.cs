4de8a77c19723eee758829abdd4434c3ba1a2d62
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-05-12 15:43:33 -0300

    feat(CreateItemDTO): add [Required] validation to Name and Description properties

--- Application/DTOs/Items/CreateITemDTO.cs
+++ Application/DTOs/Items/CreateITemDTO.cs
@@ -9,9 +9,11 @@ namespace Application.DTOs.Items
 {
     public class CreateItemDTO
     {
+        [Required]
         [StringLength(100)]
         public string? Name { get; set; }
 
+        [Required]
         [StringLength(100)]
         public string? Description { get; set; }
     }
