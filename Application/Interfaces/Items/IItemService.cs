ebb014e0475237d3191ae1ec28b91fccc41787d8
Daniel <147325621+DanielSTShimabukuro@users.noreply.github.com>
2025-05-12 15:03:51 -0300

    feat(IItemService): add CreateItem function

---
+++ Application/Interfaces/Items/IItemService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.DTOs.Items;
+using Domain.Entities.Items;
+
+namespace Application.Interfaces.Items
+{
+    public interface IItemService
+    {
+        Task<Item> CreateItem(CreateItemDTO request);
+    }
+}
